6cb9d94f6d83aacb475cae6c6264246667116fa8
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-09-05 16:23:48 +0100

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    // Fetch countries from the RESTCountries API
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const toggleVisited = (country) => {
+    if (visitedCountries.includes(country)) {
+      // Remove the country from the visited list
+      setVisitedCountries(visitedCountries.filter((c) => c !== country));
+    } else {
+      // Add the country to the visited list
+      setVisitedCountries([...visitedCountries, country]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Countries</h2>
+        <ul>
+          {countries.map((country) => (
+            <li key={country.name.common}>
+              <span
+                className={visitedCountries.includes(country.name.common) ? 'visited' : ''}
+                onClick={() => toggleVisited(country.name.common)}
+              >
+                {country.name.common}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Visited Countries</h2>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country}>{country}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
